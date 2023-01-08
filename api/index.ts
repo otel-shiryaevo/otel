7f53bbdd88fd6bf8bc27673345ffa6ffa7d9b42f
Vyacheslav Borisov <sl.borisov.42@outlook.com>
2023-01-08 14:38:31 +0400

    init

---
+++ api/index.ts
@@ -0,0 +1,24 @@
+import * as http from "http";
+import fetch from "node-fetch";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: http.IncomingMessage, res: http.ServerResponse) => {
+  console.log("sample log from sample-project");
+
+  fetch("https://api.stormkit.io", {
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: null,
+    method: "GET",
+  }).then(async (data) => {
+    const text = await data.text();
+
+    res.setHeader("Content-Type", "application/json");
+    res.write(
+      JSON.stringify({
+        apiVersion: text.split("Version: ")[1].split(/\s/)[0],
+      })
+    );
+
+    res.end();
+  });
+};
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+// eslint-disable-next-line no-unused-vars
+function generateApiResponse(response) {
+  return `{
+  "api-docs": "${response.docs}"
+}`;
+}
+
+function App() {
+
+  return;
+}
+
+export default App;
